18e71a12b5f4fd11822d3e6b641bd99316acdc39
Maxim Kozlov <Maximusvin@gmail.com>
2021-03-25 19:46:46 +0200

    fix component C

--- src/components/Task2/C.js
+++ src/components/Task2/C.js
@@ -1,13 +1,14 @@
 import s from './Task2.module.css';
 
 const C = ({ count, onGetNumber }) => {
+  const factor = 1000;
   return (
     <div className={s.c}>
       <h2 className={s.subtitle}>Компонент C</h2>
       <button
         className={s.button}
         type="button"
-        onClick={() => onGetNumber((Math.random() * 100).toFixed(1))}
+        onClick={() => onGetNumber((Math.random() * 100).toFixed(1) * 1000)}
       >
         {count ? `RandomCount: ${count}` : 'Get random count'}
       </button>
